e52d9a69f34f4c655485d77352f3ff9c4d118e63
Phillipe Rodriguez <philliperod@gmail.com>
2020-11-29 13:19:57 -0600

    created an async function to fetch openweather API; console.log the results to test if it works (successful)

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,20 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navbar } from './NavbarElements';
+// const url = 'api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '3c005018751f1dcc72c1d0f6c2e92f7b';
 
 const Nav = () => {
-  const [location, setLocation] = useState('');
-  //   const [error, setError] = useState(false);
+  const [location, setLocation] = useState('76542');
 
-  //   const getLocationWeather = async () => {
-  //     const apiKey = '3c005018751f1dcc72c1d0f6c2e92f7b';
-  //     const response = await fetch(
-  //       `https://api.openweathermap.org/data/2.5/weather?zip=${location},us&appid=${apiKey}&units=metric`
-  //     );
+  const getLocationWeather = async (location) => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?zip=${location},us&appid=${apiKey}&units=imperial`
+    );
+    const result = await response.json();
+    console.log(result);
 
-  //     if (response.status === 200) {
-  //       return { success: true };
-  //     }
-  //   };
+    // if (response.status === 200) {
+    //     return { success: true };
+    // }
+  };
+
+  useEffect(() => {
+    getLocationWeather(location);
+  }, [location]);
 
   const handleChange = (event) => {
     event.preventDefault();
