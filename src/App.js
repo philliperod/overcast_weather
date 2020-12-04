c73f861f313653a1c67790a3874586518d2d152a
Phillipe Rodriguez <philliperod@gmail.com>
2020-12-04 11:57:27 -0600

    commented out the fetch data function; will rework to have two separate fetch data functions: one for the zip code API and one for Open Weather API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Current from './Components/Current';
 import FiveDays from './Components/FiveDays';
 import Hourly from './Components/Hourly';
@@ -6,18 +6,19 @@ import Nav from './Components/Navbar';
 import Radar from './Components/Radar';
 const dotenv = require('dotenv').config();
 const apiKey = process.env.REACT_APP_API_KEY;
+const zipApi = process.env.REACT_APP_ZIP_KEY;
 
 function App() {
-  const [zipCode, setZipCode] = useState('');
-  const [weather, setWeather] = useState([]);
+  // const [zipCode, setZipCode] = useState('');
+  // const [weather, setWeather] = useState([]);
 
-  const fetchWeather = async (zipCode) => {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}&units=imperial`
-    )
-      .then((response) => response.json())
-      .then((response) => setResponseObj(response));
-  };
+  // const fetchWeather = async (zipCode) => {
+  //   const response = await fetch(
+  //     `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}&units=imperial`
+  //   )
+  //     .then((response) => response.json())
+  //     .then((response) => setResponseObj(response));
+  // };
 
   return (
     <div className="App">
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Navbar } from './NavbarElements';
+const dotenv = require('dotenv').config();
 const apiKey = process.env.REACT_APP_API_KEY;
+const zipApi = process.env.REACT_APP_ZIP_KEY;
 
 const Nav = () => {
   const [location, setLocation] = useState([]);
@@ -9,16 +11,24 @@ const Nav = () => {
   //   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    getCurrent(location);
+    getLocation(location);
   }, [location]);
 
-  const getCurrent = async (location) => {
+  const getLocation = async (location) => {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?zip=${location},us&appid=${apiKey}&units=imperial`
+      `https://www.zipcodeapi.com/rest/${zipApi}/info.json/${location}/degrees`
     )
       .then((response) => response.json())
       .then((response) => setResponseObj(response));
   };
+
+  // const getCurrent = async (location) => {
+  //   const response = await fetch(
+  //     `https://api.openweathermap.org/data/2.5/weather?zip=${location},us&appid=${apiKey}&units=imperial`
+  //   )
+  //     .then((response) => response.json())
+  //     .then((response) => setResponseObj(response));
+  // };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -42,6 +52,7 @@ const Nav = () => {
           />
           <button type="submit">Submit</button>
           <p>{location}</p>
+          <p>{console.log(responseObj)}</p>
         </form>
         <header>
           <div className="current-weather">{JSON.stringify(responseObj)}</div>
